252c654987de2508f2aebd5e3126aafeb03ee608
pengjiankang <3136812707@qq.com>
2023-01-13 16:48:06 +0800

    hot_fix commit2

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -4,6 +4,7 @@ public class gitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot_fix test");
 
     }
 }
